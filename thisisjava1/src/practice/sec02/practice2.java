32d4cddfe2724a388decdf6136ecccab0875d6d7
DESKTOP-8LOGFNE\admin <phj2394324@naver.com>
2025-04-08 20:55:00 +0900

    java practice refactor

--- thisisjava1/src/practice/sec02/practice2.java
+++ thisisjava1/src/practice/sec02/practice2.java
@@ -3,12 +3,12 @@ package practice.sec02;
 public class practice2 {
 
 	public static void main(String[] args) {
-		10 //int형
-		1. // double형
-		6F //float형
-		23d //double형
-		14E10 //double형
-		'a' //char형
+//		10 //int형
+//		1. // double형
+//		6F //float형
+//		23d //double형
+//		14E10 //double형
+//		'a' //char형
 
 	}
 

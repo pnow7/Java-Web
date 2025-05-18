beac9658a98d10470e67a2f0bb2e8effd011c3e8
DESKTOP-8LOGFNE\admin <phj2394324@naver.com>
2025-05-18 22:47:17 +0900

    practice

---
+++ thisisjava1/src/object_oriented_practice/ch06_star/Earth.java
@@ -0,0 +1,17 @@
+package object_oriented_practice.ch06_star;
+
+public class Earth {
+	
+	static final double EARTH_RADIUS = 6400;
+	static final double EARTH_SURFACE_AREA;
+	
+	static {
+		EARTH_SURFACE_AREA = 4 * Math.PI * EARTH_RADIUS;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("지구의 반지름 : " + Earth.EARTH_RADIUS + "km");
+		System.out.println("지구의 표면적 : " + Earth.EARTH_SURFACE_AREA + "km^2");
+	}
+	
+}
---
+++ thisisjava1/src/object_oriented_practice/ch06_star/MiniCar.java
@@ -0,0 +1,43 @@
+package object_oriented_practice.ch06_star;
+
+public class MiniCar {
+	int gas;
+	
+	void setGas(int gas) {
+		this.gas = gas;
+	}
+	
+	boolean isLeftGas() {
+		if(gas == 0) {
+			System.out.println("gas가 없음");
+			return false;
+		}
+		System.out.println("gas가 있음");
+		return true;
+	}
+	
+	void run() {
+		while(true) {
+			if(gas > 0) {
+				System.out.println("달림. (gas 잔량 : " + gas + " )");
+				gas -= 1;
+			}
+			else {
+				System.out.println("멈춤. (gas 잔량 : " + gas + " )");
+				return;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		MiniCar car = new MiniCar();
+		
+		car.setGas(10);
+		
+		if(car.isLeftGas()) {
+			System.out.println("\n출발");
+			car.run();
+		}
+		System.out.println("\ngas를 넣어");
+	}
+}

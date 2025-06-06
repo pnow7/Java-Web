f24563092dcf93814eb3bd90ce7a608c5b745ec4
DESKTOP-8LOGFNE\admin <phj2394324@naver.com>
2025-06-06 17:07:45 +0900

    boj

--- thisisjava1/src/study/DFS1.java
+++ thisisjava1/src/study/DFS1.java
@@ -3,7 +3,17 @@ package study;
 public class DFS1 {
 	//방문처리에 사용 할 배열선언
 	static boolean[] visited = new boolean[9];
-	static int[][] tree = {{}, {2,3,8}, {1,6,8},{1,5},{5,7},{3,4,7},{2},{4,5},{1,2}};
+	static int[][] tree = {
+			{}, 
+			{2,3,8}, 
+			{1,6,8},
+			{1,5},
+			{5,7},
+			{3,4,7},
+			{2},
+			{4,5},
+			{1,2}
+	};
 
 	public static void main(String[] args) {
 		dfs(7); //정수 부터 시작

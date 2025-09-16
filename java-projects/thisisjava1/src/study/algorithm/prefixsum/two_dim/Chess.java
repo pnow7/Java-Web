49ca6eb37c8fb9a0ec49c5df7cd14fe0b7bd2a1b
guswo4324 <phj2394324@naver.com>
2025-09-16 15:26:10 +0900

    [boj] Chess prefixSum

--- java-projects/thisisjava1/src/study/algorithm/prefixsum/two_dim/Chess.java
+++ java-projects/thisisjava1/src/study/algorithm/prefixsum/two_dim/Chess.java
@@ -1,4 +1,4 @@
-package test.test09.test0915;
+package study.algorithm.prefixsum.two_dim;
 
 /*
 
@@ -31,7 +31,7 @@ WWWWWWWWWWBWB
 import java.io.*;
 import java.util.*;
 
-public class Main25682 {
+public class Chess {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -69,8 +69,12 @@ public class Main25682 {
 			}
 		}
 		
+		// result 변수 없애고 min만 쓰면 반복문 끝났을 때 min에는 마지막으로 계산된 K x K 사각형의 값만 남아있기때문에
+		// result 변수를 선언하고 min값 중 가장 작은 값 출력하기 위함
 		int min = 0;
 		int result = Integer.MAX_VALUE;
+		// N x M 체스판에서 K x K 사각형 반복 횟수
+		// ex) 8 8 8 이면 8 x 8체스판에서 8 x 8 사각형은 하나 밖에 없으므로 한번만 돌아감 
 		for (int i = K; i <= N; i++) {
 			for (int j = K; j <= M; j++) {
 				min = Math.min(w_sum[i][j] - w_sum[i - K][j] - w_sum[i][j - K] + w_sum[i - K][j - K], b_sum[i][j] - b_sum[i][j - K] - b_sum[i - K][j] + b_sum[i - K][j - K]);

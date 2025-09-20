539dd3f99c43e634d8f02f89247704c5ba60a4cf
guswo4324 <phj2394324@naver.com>
2025-09-20 20:01:31 +0900

    [boj] Greedy - Gas_Station #1

---
+++ java-projects/thisisjava1/src/study/algorithm/greedy/Gas_station.java
@@ -0,0 +1,59 @@
+package study.algorithm.greedy;
+
+/*
+- 주유소
+
+[입력]
+4
+2 3 1
+5 2 4 1
+
+4
+3 3 4
+1 1 1 1
+
+[출력]
+18
+
+10
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Gas_station {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] distance = new int[N - 1];
+		int[] price = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N - 1; i++) {
+			distance[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			price[i] = Integer.parseInt(st1.nextToken());
+		}
+		
+		long total = (long)price[0] * distance[0];
+		long min_price = price[0];
+		
+		for (int i = 1; i < N - 1; i++) {
+			if (min_price > price[i]) {
+				min_price = price[i];
+			}
+			total += min_price * distance[i];
+		}
+		
+		System.out.println(total);
+		br.close();
+	}
+	
+}
